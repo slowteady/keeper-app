53926eedadf00e3d4c3d54877913bf5edb51d806
Lee Yong Min <vbn0213@gmail.com>
2025-02-03 22:58:29 +0900

    CHORE: 불필요한 파일 제거

--- services/bannersService.ts
+++
@@ -1,8 +0,0 @@
-import { ApiResponse } from '@/types/common';
-import { BannerData } from '@/types/scheme/banners';
-import { publicApi } from './instance';
-
-export const getBanners = async (): Promise<ApiResponse<BannerData[]>> => {
-  const endpoint = '/banners';
-  return publicApi({ endpoint, options: { method: 'GET' } });
-};
