6f5dcb17c29c54fd2bb8a7beddce145def1dd3f1
Lee Yong Min <vbn0213@gmail.com>
2025-02-16 22:21:24 +0900

    FEAT: 메인화면 보호소 맵 기능 구현

--- components/organisms/map/ShelterMap.tsx
+++ components/organisms/map/ShelterMap.tsx
@@ -1,93 +1,92 @@
 import theme from '@/constants/theme';
-import { Camera, CameraChangeReason, NaverMapView, NaverMapViewRef, Region } from '@mj-studio/react-native-naver-map';
+import { ShelterValue } from '@/types/scheme/shelters';
+import {
+  Camera,
+  CameraChangeReason,
+  NaverMapMarkerOverlay,
+  NaverMapView,
+  NaverMapViewProps,
+  NaverMapViewRef,
+  Region
+} from '@mj-studio/react-native-naver-map';
 import { applicationId } from 'expo-application';
+import * as Haptics from 'expo-haptics';
 import { ActivityAction, startActivityAsync } from 'expo-intent-launcher';
-import * as Location from 'expo-location';
-import { useEffect, useRef, useState } from 'react';
+import { forwardRef, useRef, useState } from 'react';
 import { Dimensions, Linking, Platform, StyleSheet, Text, TouchableOpacity, View, ViewStyle } from 'react-native';
 
-const Map = () => {
-  const [status, requestPermission] = Location.useForegroundPermissions();
-  const [camera, setCamera] = useState<Camera>();
-  const mapRef = useRef<NaverMapViewRef | null>(null);
+export interface ShelterMapProps extends NaverMapViewProps {
+  hasLocation: boolean;
+  data?: ShelterValue[];
+  onRefetch: (camera?: Camera) => void;
+  onTapMarker?: (data: ShelterValue) => void;
+  selectedMarkerId?: number;
+}
+const Map = forwardRef<NaverMapViewRef, ShelterMapProps>(
+  ({ hasLocation, data, onRefetch, onTapMarker, selectedMarkerId, ...props }, ref) => {
+    const [isVisibleButton, setIsVisibleButton] = useState(false);
+    const cameraRef = useRef<Camera>();
 
-  useEffect(() => {
-    const getCurrentLocation = async () => {
-      const response = await requestPermission();
+    const handleChangeCamera = (
+      params: Camera & {
+        reason: CameraChangeReason;
+        region: Region;
+      }
+    ) => {
+      setIsVisibleButton(true);
+      cameraRef.current = params;
+    };
 
-      if (response.status === Location.PermissionStatus.GRANTED) {
-        const { coords } = await Location.getCurrentPositionAsync({});
+    const handlePressRefetch = async () => {
+      await Haptics.selectionAsync();
+      setIsVisibleButton(false);
+      onRefetch(cameraRef.current);
+    };
 
-        mapRef.current?.animateCameraTo({
-          latitude: coords.latitude,
-          longitude: coords.longitude,
-          zoom: 13
-        });
-        mapRef.current?.setLocationTrackingMode('Follow');
+    const handleTapMarker = (data: ShelterValue) => {
+      if (ref && 'current' in ref && ref.current) {
+        ref?.current?.animateCameraTo({ latitude: data.latitude, longitude: data.longitude });
+        onTapMarker?.(data);
       }
     };
 
-    getCurrentLocation();
-  }, [requestPermission]);
-
-  const handlePressSetting = async () => {
-    if (Platform.OS === 'ios') {
-      Linking.openURL('app-settings:');
-    } else if (Platform.OS === 'android') {
-      startActivityAsync(ActivityAction.APPLICATION_DETAILS_SETTINGS, {
-        data: `package:${applicationId}`
-      });
-    }
-  };
-
-  const handleTapLeaf = () => {
-    // NOTE: 리프 마커 클릭 시 -> 보호소 배열 정렬 1등으로 나오도록
-  };
-
-  const handleChangeCamera = (
-    params: Camera & {
-      reason: CameraChangeReason;
-      region: Region;
-    }
-  ) => {
-    setCamera(params);
-  };
-
-  const hasLocation = Boolean(status) && status?.status === Location.PermissionStatus.GRANTED;
-
-  return (
-    <View style={[styles.container]}>
-      {hasLocation ? (
-        <NaverMapView
-          camera={camera}
-          ref={mapRef}
-          onTapClusterLeaf={handleTapLeaf}
-          onCameraChanged={handleChangeCamera}
-          isExtentBoundedInKorea
-          isShowLocationButton={true}
-          isShowScaleBar={false}
-          isShowZoomControls={false}
-          minZoom={13}
-          maxZoom={13}
-          isZoomGesturesEnabled={false}
-          style={styles.mapContainer}
-        ></NaverMapView>
-      ) : (
-        <View style={styles.requestContainer}>
-          <Text>위치 권한 설정이 완료되지 않았습니다.</Text>
-          <Text>{`설정 > Keeper > 위치 접근 허용을 설정해주세요.`}</Text>
-          <TouchableOpacity
-            style={{ padding: 20, borderRadius: 10, backgroundColor: theme.colors.black[500] }}
-            activeOpacity={0.5}
-            onPress={handlePressSetting}
-          >
-            <Text>설정하기</Text>
-          </TouchableOpacity>
-        </View>
-      )}
-    </View>
-  );
-};
+    return (
+      <View style={[styles.container]}>
+        {hasLocation ? (
+          <>
+            <NaverMapView
+              ref={ref}
+              onCameraChanged={handleChangeCamera}
+              isExtentBoundedInKorea
+              minZoom={11}
+              isShowCompass={false}
+              isShowScaleBar={false}
+              isShowLocationButton={true}
+              isShowZoomControls={true}
+              isZoomGesturesEnabled={true}
+              isTiltGesturesEnabled={false}
+              isRotateGesturesEnabled={true}
+              animationDuration={0.5}
+              style={styles.mapContainer}
+              {...props}
+            >
+              {data?.map((item) => (
+                <Marker data={item} key={item.id} onTap={handleTapMarker} isSelectedId={selectedMarkerId} />
+              ))}
+            </NaverMapView>
+            {isVisibleButton && (
+              <TouchableOpacity style={styles.mapButton} activeOpacity={0.5} onPress={handlePressRefetch}>
+                <Text style={styles.mapButtonText}>현 지도에서 검색</Text>
+              </TouchableOpacity>
+            )}
+          </>
+        ) : (
+          <NoValidMap />
+        )}
+      </View>
+    );
+  }
+);
 
 export interface ShelterMapDistanceBoxProps {
   value: {
@@ -120,8 +119,64 @@ const DistanceBox = ({ value, style }: ShelterMapDistanceBoxProps) => {
   );
 };
 
+interface ShelterMapMarkerProps {
+  data: ShelterValue;
+  onTap?: (data: ShelterValue) => void;
+  isSelectedId?: number;
+}
+const MARKER_DEFAULT_ZINDEX = 200000;
+const Marker = ({ data, onTap, isSelectedId }: ShelterMapMarkerProps) => {
+  const { id, latitude, longitude } = data;
+
+  const handleTapMarker = () => {
+    onTap?.(data);
+  };
+
+  const isSelected = isSelectedId === id;
+
+  return (
+    <NaverMapMarkerOverlay
+      image={require('@/assets/images/marker.png')}
+      latitude={latitude}
+      longitude={longitude}
+      height={isSelected ? 42 : 32}
+      width={isSelected ? 38 : 28}
+      onTap={handleTapMarker}
+      caption={isSelected ? { text: data.name, textSize: 13 } : undefined}
+      globalZIndex={isSelected ? MARKER_DEFAULT_ZINDEX + 1 : MARKER_DEFAULT_ZINDEX}
+    />
+  );
+};
+
+const NoValidMap = () => {
+  const handlePressSetting = async () => {
+    if (Platform.OS === 'ios') {
+      Linking.openURL('app-settings:');
+    } else if (Platform.OS === 'android') {
+      startActivityAsync(ActivityAction.APPLICATION_DETAILS_SETTINGS, {
+        data: `package:${applicationId}`
+      });
+    }
+  };
+
+  return (
+    <View style={styles.requestContainer}>
+      <Text>위치 권한 설정이 완료되지 않았습니다.</Text>
+      <Text>{`설정 > Keeper > 위치 접근 허용을 설정해주세요.`}</Text>
+      <TouchableOpacity
+        style={{ padding: 20, borderRadius: 10, backgroundColor: theme.colors.black[500] }}
+        activeOpacity={0.5}
+        onPress={handlePressSetting}
+      >
+        <Text>설정하기</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
 export const ShelterMap = Object.assign(Map, {
-  DistanceBox
+  DistanceBox,
+  NoValidMap
 });
 
 const styles = StyleSheet.create({
@@ -129,14 +184,15 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: '#D9D9D9',
     width: '100%',
-    height: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').width * 0.8,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center'
   },
   mapContainer: {
     width: '100%',
-    height: '100%'
+    height: '100%',
+    position: 'relative'
   },
   requestContainer: {
     display: 'flex',
@@ -170,5 +226,35 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: '700',
     lineHeight: 22
+  },
+  mapButton: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 10,
+    borderRadius: 24,
+    backgroundColor: theme.colors.primary.main,
+    paddingHorizontal: 18,
+    height: 40,
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: { width: 1, height: 1 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3
+      },
+      android: {
+        elevation: 4
+      }
+    })
+  },
+  mapButtonText: {
+    color: theme.colors.black[900],
+    fontSize: 13,
+    fontWeight: '600',
+    lineHeight: 22
   }
 });
+
+Map.displayName = 'ShelterMap';
--- components/sections/main/MainShelterSection.tsx
+++ components/sections/main/MainShelterSection.tsx
@@ -1,12 +1,71 @@
 import { NavArrowIcon } from '@/components/atoms/icons/ArrowIcon';
 import { ShelterMap } from '@/components/organisms/map/ShelterMap';
 import theme from '@/constants/theme';
+import { useGetShelters } from '@/hooks/queries/useShelters';
+import { ShelterValue } from '@/types/scheme/shelters';
+import { Camera, NaverMapViewRef } from '@mj-studio/react-native-naver-map';
+import * as Location from 'expo-location';
 import { router } from 'expo-router';
+import { useEffect, useRef, useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 const MainShelterSection = () => {
+  const [camera, setCamera] = useState<Camera>();
+  const [enabled, setEnabled] = useState(false);
+  const [selectedMarkerId, setSelectedMarkerId] = useState(0);
+  const [shelterData, setShelterData] = useState<ShelterValue[]>([]); // ✅ 변경된 부분
+  const mapRef = useRef<NaverMapViewRef | null>(null);
+  const [status, requestPermission] = Location.useForegroundPermissions();
+
+  const { data } = useGetShelters(
+    { latitude: camera?.latitude || 0, longitude: camera?.longitude || 0, radius: 50 },
+    { enabled: Boolean(camera) && enabled }
+  );
+
+  useEffect(() => {
+    const getCurrentLocation = async () => {
+      const response = await requestPermission();
+
+      if (response.status === Location.PermissionStatus.GRANTED) {
+        const { coords } = await Location.getCurrentPositionAsync({});
+        setCamera(coords);
+
+        mapRef.current?.animateCameraTo({
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+          zoom: 13
+        });
+        mapRef.current?.setLocationTrackingMode('Follow');
+      }
+    };
+
+    getCurrentLocation();
+  }, [requestPermission]);
+
+  useEffect(() => {
+    if (!data) return;
+    setShelterData(data);
+  }, [data]);
+
   const handlePressButton = () => {
     router.push('/shelters');
+  };
+
+  const handleRefetch = (camera?: Camera) => {
+    setCamera(camera);
+  };
+
+  const handleTapMarker = (data: ShelterValue) => {
+    setSelectedMarkerId(data.id);
+    setShelterData((prev) => [data, ...prev.filter((item) => item.id !== data.id)]);
+  };
+
+  const handleTapMap = () => {
+    setSelectedMarkerId(0);
+  };
+
+  const handleInitMap = () => {
+    setEnabled(true);
   };
 
   const distanceValue = {
@@ -15,6 +74,8 @@ const MainShelterSection = () => {
     '30km': 0,
     '50km': 0
   };
+
+  const hasLocation = Boolean(status) && status?.status === Location.PermissionStatus.GRANTED;
 
   return (
     <View style={styles.container}>
@@ -28,7 +89,17 @@ const MainShelterSection = () => {
 
       <View style={{ paddingHorizontal: 20, flex: 1 }}>
         <ShelterMap.DistanceBox style={{ marginBottom: 16 }} value={distanceValue} />
-        <ShelterMap />
+        <ShelterMap
+          ref={mapRef}
+          hasLocation={hasLocation}
+          data={shelterData}
+          camera={camera}
+          onRefetch={handleRefetch}
+          onTapMarker={handleTapMarker}
+          onTapMap={handleTapMap}
+          selectedMarkerId={selectedMarkerId}
+          onInitialized={handleInitMap}
+        />
       </View>
     </View>
   );
