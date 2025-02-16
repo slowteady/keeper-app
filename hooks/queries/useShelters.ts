48ed5c5fe74d22d0aa93a9534699c044e3829a47
Lee Yong Min <vbn0213@gmail.com>
2025-02-16 22:21:12 +0900

    FEAT: 데이터 패칭 로직 추가

--- hooks/queries/useShelters.ts
+++ hooks/queries/useShelters.ts
@@ -1,12 +1,29 @@
 import { SHELTER_QUERY_KEY } from '@/constants/queryKeys';
-import { getShelter } from '@/services/sheltersService';
+import { getShelter, getShelters, GetSheltersParams } from '@/services/sheltersService';
 import { ApiResponse } from '@/types/common';
 import { ShelterValue } from '@/types/scheme/shelters';
 import { UseQueryCustomOptions } from '@/types/utils';
 import { useQuery } from '@tanstack/react-query';
 
 /**
- * 보호소 상세
+ * 보호소 전체 조회
+ * @param queryOptions
+ * @returns
+ */
+export const useGetShelters = (
+  params: GetSheltersParams,
+  queryOptions?: UseQueryCustomOptions<ApiResponse<ShelterValue[]>, Error, ShelterValue[]>
+) => {
+  return useQuery<ApiResponse<ShelterValue[]>, Error, ShelterValue[]>({
+    queryKey: [SHELTER_QUERY_KEY, params],
+    queryFn: () => getShelters(params),
+    select: (data) => data.data,
+    ...queryOptions
+  });
+};
+
+/**
+ * 보호소 상세 조회
  * @param id
  * @param queryOptions
  * @returns
--- mocks/shelters/index.ts
+++ mocks/shelters/index.ts
@@ -1,6 +1,13 @@
 import { http, HttpResponse } from 'msw';
 import sheltersData from './shelters.json';
 
+export const shelters = http.get('https://app.our-keeper.com/api/shelters', ({ params }) => {
+  return HttpResponse.json({
+    code: 'OK',
+    data: sheltersData
+  });
+});
+
 export const shelter = http.get('https://app.our-keeper.com/api/shelters/:id', ({ params }) => {
   const { id } = params;
   const shelterValue = sheltersData.find((item) => item.id === Number(id));
