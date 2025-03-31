53adfb48c0abffdc1f9019778b89955419b64b82
Yongmin Lee <ymlee.dev@gmail.com>
2025-03-31 14:00:56 +0900

    FEAT: 아이콘 추가

--- src/app/login/index.tsx
+++ src/app/login/index.tsx
@@ -1,5 +1,5 @@
 import Button from '@/components/atoms/button/Button';
-import { Kakao } from '@/components/atoms/icons/etc';
+import { Apple, Google, Kakao, Naver } from '@/components/atoms/icons/etc';
 import theme from '@/constants/theme';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -7,22 +7,33 @@ const Page = () => {
   return (
     <View style={styles.container}>
       <View style={[styles.cFlex, { marginBottom: 48 }]}>
-        <Text style={styles.title}>소셜 로그인</Text>
+        <Text style={styles.title}>Social Login</Text>
         <Text style={styles.subTitle}>소셜로그인으로 Keeper와 함께하세요.</Text>
       </View>
 
       <View style={styles.cFlex}>
         <Button style={[styles.button, { backgroundColor: '#FEE500' }]}>
-          <Kakao width={24} height={24} />
+          <View style={styles.iconWrap}>
+            <Kakao width={22} height={22} color={theme.colors.black[900]} />
+          </View>
           <Text style={[styles.buttonText]}>Login with Kakao</Text>
         </Button>
         <Button style={[styles.button, { backgroundColor: '#03C75A' }]}>
+          <View style={styles.iconWrap}>
+            <Naver width={22} height={22} />
+          </View>
           <Text style={[styles.buttonText, { color: theme.colors.white[900] }]}>네이버로 시작하기</Text>
         </Button>
         <Button style={[styles.button, { backgroundColor: '#FFFFFF' }]}>
+          <View style={styles.iconWrap}>
+            <Google width={22} height={22} />
+          </View>
           <Text style={[styles.buttonText, { color: theme.colors.black[900], opacity: 0.54 }]}>구글로 시작하기</Text>
         </Button>
         <Button style={[styles.button, { backgroundColor: '#000000' }]}>
+          <View style={styles.iconWrap}>
+            <Apple width={22} height={22} />
+          </View>
           <Text style={[styles.buttonText, { color: theme.colors.white[900] }]}>애플로 시작하기</Text>
         </Button>
       </View>
@@ -58,6 +69,7 @@ const styles = StyleSheet.create({
     color: theme.colors.black[600]
   },
   button: {
+    position: 'relative',
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
@@ -70,6 +82,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '600',
     lineHeight: 18,
-    marginLeft: 18
+    marginLeft: 18,
+    flex: 1
+  },
+  iconWrap: {
+    flexBasis: '30%',
+    alignItems: 'flex-end'
   }
 });
--- src/components/atoms/icons/outline/Home.tsx
+++ src/components/atoms/icons/outline/Home.tsx
@@ -1,5 +1,6 @@
+import * as React from 'react';
+import Svg, { Mask, Rect, G, Path } from 'react-native-svg';
 import type { SvgProps } from 'react-native-svg';
-import Svg, { G, Mask, Path, Rect } from 'react-native-svg';
 const SvgHome = (props: SvgProps) => (
   <Svg width={24} height={24} viewBox="0 0 25 24" fill="none" {...props}>
     <Mask
@@ -18,7 +19,7 @@ const SvgHome = (props: SvgProps) => (
     <G mask="url(#mask0_549_8738)">
       <Path
         d="M5 20.5556V20.8056H5.25H9H9.25V20.5556V13.4722H16.25V20.5556V20.8056H16.5H20.25H20.5V20.5556V9.55556V9.42887L20.3978 9.35395L12.8978 3.85395L12.75 3.74554L12.6022 3.85395L5.10216 9.35395L5 9.42887V9.55556V20.5556ZM22.5 8.46002V22.75H14.25V15.6667V15.4167H14H11.5H11.25V15.6667V22.75H3V8.46002L12.75 1.31002L22.5 8.46002Z"
-        fill="currentColor"
+        fill="#161717"
         stroke="#F3F4F4"
         strokeWidth={0.5}
       />
