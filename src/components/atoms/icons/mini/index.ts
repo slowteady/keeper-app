2bc46307b7b6d0a81718df925a7e888dd8450f43
Lee Yong Min <vbn0213@gmail.com>
2025-05-10 14:59:00 +0900

    CHORE: 아이콘 추가

--- src/components/atoms/icons/mini/index.ts
+++ src/components/atoms/icons/mini/index.ts
@@ -1,3 +1,6 @@
 export { default as DownArrow } from './DownArrow';
+export { default as Favorite } from './Favorite';
 export { default as LeftLineArrow } from './LeftLineArrow';
+export { default as MarkChatUnread } from './MarkChatUnread';
 export { default as RightLineArrow } from './RightLineArrow';
+export { default as Visibility } from './Visibility';
