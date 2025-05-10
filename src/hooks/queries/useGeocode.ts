b574ca142b86938802debbb382f54a51350ba942
Lee Yong Min <vbn0213@gmail.com>
2025-05-10 18:01:54 +0900

    CHORE: 불필요한 코드 및 환경변수 정리

--- src/hooks/queries/useGeocode.ts
+++ src/hooks/queries/useGeocode.ts
@@ -1,17 +1,7 @@
-import { GeocodeParams, getGeocode, getKakaoGeocode, KakaoGeocodeParams } from '@/services/geocodeService';
-import { GeocodeResponse, KakaoGeocodeResponse } from '@/types/map';
+import { getKakaoGeocode, KakaoGeocodeParams } from '@/services/geocodeService';
+import { KakaoGeocodeResponse } from '@/types/map';
 import { UseMutationCustomOptions } from '@/types/utils';
 import { useMutation } from '@tanstack/react-query';
-
-export const useGeocodeMutation = (
-  mutationOptions?: UseMutationCustomOptions<GeocodeResponse, Error, GeocodeParams>
-) => {
-  return useMutation<GeocodeResponse, Error, GeocodeParams>({
-    mutationFn: (params) => getGeocode(params),
-    throwOnError: true,
-    ...mutationOptions
-  });
-};
 
 export const useKakaoGeocodeMutation = (
   mutationOptions?: UseMutationCustomOptions<KakaoGeocodeResponse, Error, KakaoGeocodeParams>
