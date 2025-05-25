204602ad29684bdb38e24a6e49e342aad2a68915
Lee Yong Min <vbn0213@gmail.com>
2025-05-25 20:52:09 +0900

    CHORE: 배포를 위한 버전 업

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default () => ({
   expo: {
     name: 'keeper',
     slug: 'keeper',
-    version: '1.3.3',
+    version: '1.3.4',
     orientation: 'portrait',
     scheme: 'keeper',
     userInterfaceStyle: 'automatic',
