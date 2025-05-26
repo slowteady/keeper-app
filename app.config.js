571de6113a00fddca5d0713320fbf333216533b3
Lee Yong Min <vbn0213@gmail.com>
2025-05-26 17:28:44 +0900

    CHORE: 1.3.7 업데이트

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default () => ({
   expo: {
     name: 'keeper',
     slug: 'keeper',
-    version: '1.3.6',
+    version: '1.3.7',
     orientation: 'portrait',
     scheme: 'keeper',
     userInterfaceStyle: 'automatic',
