7700eebdccd92635258b59586cffe37f542bfb1c
Lee Yong Min <vbn0213@gmail.com>
2025-06-20 23:01:09 +0900

    CHORE: lint 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,19 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
-  ignorePatterns: ['/dist/*']
+  extends: ['expo', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript'],
+  ignorePatterns: ['/dist/*'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json'
+      },
+      node: {
+        moduleDirectory: ['node_modules', 'src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  rules: {
+    'import/no-unresolved': 'error'
+  }
 };
