e44499176df748854ab1206c4ff75f805917b4da
Lee Yong Min <vbn0213@gmail.com>
2025-08-26 21:40:17 +0900

    CHORE: eslint 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   extends: ['expo', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript'],
   ignorePatterns: ['/dist/*'],
+  plugins: ['simple-import-sort', 'unused-imports'],
   settings: {
     'import/resolver': {
       typescript: {
@@ -14,6 +15,18 @@ module.exports = {
     }
   },
   rules: {
-    'import/no-unresolved': 'error'
+    'import/no-unresolved': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ]
   }
 };
