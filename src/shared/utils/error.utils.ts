6b57895acdd8fdfc8bc81b07cbdbb249033e7263
yong-min-lee <yongmin.lee@yaksonhealthcare.com>
2025-09-04 18:14:37 +0900

    CHORE: 로거 유틸 함수 추가

--- src/shared/utils/error.utils.ts
+++ src/shared/utils/error.utils.ts
@@ -1,10 +1,49 @@
 import { AxiosError } from 'axios';
 
+/**
+ * 개발/운영 환경에 따른 로거
+ */
+export const logger = {
+  log: (...args: any[]) => {
+    if (__DEV__) {
+      console.log(...args);
+    }
+  },
+  warn: (...args: any[]) => {
+    if (__DEV__) {
+      console.warn(...args);
+    }
+  },
+  error: (...args: any[]) => {
+    if (__DEV__) {
+      console.error(...args);
+    } else {
+      // 운영환경에서는 에러 로깅 서비스에 전송 (예: Sentry, Crashlytics 등)
+      // 여기서는 콘솔에만 출력하되 더 간단하게
+      console.error('[ERROR]', ...args);
+    }
+  },
+  debug: (...args: any[]) => {
+    if (__DEV__) {
+      console.debug('[DEBUG]', ...args);
+    }
+  },
+  info: (...args: any[]) => {
+    if (__DEV__) {
+      console.info('[INFO]', ...args);
+    }
+  }
+};
+
 export const handleError = (error: unknown, msg: string): Error => {
   if (error instanceof AxiosError) {
-    console.error(`${msg}: ${error.message}`);
+    logger.error(`${msg}: ${error.message}`, {
+      response: error.response,
+      config: error.config
+    });
+
     throw error;
   }
-  console.error(error);
+
   throw error;
 };
