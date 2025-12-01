87da79b617cf0812f8cc3891844cc536ffb7e156
Lee Yong Min <vbn0213@gmail.com>
2025-12-01 23:03:08 +0900

    CHORE: 보호소 디테일 진행중

--- src/widgets/shelter-detail-section/ui/ShelterDetailOverviewSection.tsx
+++ src/widgets/shelter-detail-section/ui/ShelterDetailOverviewSection.tsx
@@ -1,9 +1,17 @@
-import { Text, View } from 'tamagui';
+import { Text, YStack } from 'tamagui';
+
+export interface ShelterDetailOverviewSectionProps {
+  title: string;
+  images: string[];
+  description: { label: string; value: string }[];
+}
 
 export const ShelterDetailOverviewSection = () => {
   return (
-    <View>
-      <Text>ShelterDetailOverviewSection</Text>
-    </View>
+    <YStack>
+      <Text fontSize={28} lineHeight={38} fontWeight="500" color="$black900">
+        ShelterDetailOverviewSection
+      </Text>
+    </YStack>
   );
 };
